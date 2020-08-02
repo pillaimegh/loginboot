1ae9cdc1a2769917b6c4142e4af0763aa7fa0190
pillaimegh <meghnapillai3@gmail.com>
2020-08-02 17:05:03 +0530

    initial proj structure

---
+++ src/main/java/com/lti/LoginBoot/LoginBootApplication.java
@@ -0,0 +1,13 @@
+package com.lti.LoginBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginBootApplication.class, args);
+	}
+
+}
